50568853bb81219602820dfd93b42371c4185f64
Nea Noc <nea.a.noc@gmail.com>
2022-11-15 12:09:16 -0500

    We set up the user model with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # we need feedback here
+  # config.action_mailer.default_url_options = { host: 'heroku', port: 8080 }
 end
