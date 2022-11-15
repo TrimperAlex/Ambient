69fd012ef9d59a8bc50b45cdac76b80b1b75d200
Suraj Gopal <surajcancode@gmail.com>
2022-11-15 17:27:41 -0500

    added all basic routes to WE controller

--- app/controllers/weather_events_controller.rb
+++ app/controllers/weather_events_controller.rb
@@ -1,2 +1,37 @@
 class WeatherEventsController < ApplicationController
+  before_action :set_weather_event, only: %i[show update destroy]
+  def index
+    @weather_events = WeatherEvent.all
+  end
+
+  def new
+    @weather_event = WeatherEvent.new
+  end
+
+  def show
+  end
+
+  def create
+    @weather_event = WeatherEvent.new(weather_event_params)
+
+    if @weather_event.save
+      redirect_to weather_event_path(@weather_event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @weather_event.destroy
+  end
+
+  private
+
+  def set_weather_event
+    @weather_event = WeatherEvent.find(params[:id])
+  end
+
+  def weather_event_params
+    params.require(:weather_event).permit(:description, :category, :temperature, :image_url, :price, :owner)
+  end
 end
