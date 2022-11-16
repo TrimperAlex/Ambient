4c0d8f8734945863055c6276cfe96c36b84bf420
Suraj Gopal <surajcancode@gmail.com>
2022-11-16 12:19:59 -0500

    completed routes for app

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show]
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to
+    else
+
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :location, :renter, :weather_event)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :weather_events
+  resources :weather_events do
+    resources :bookings, only: %i[new create edit update]
+  end
+
+  resources :bookings, only: %i[destroy show]
 end
